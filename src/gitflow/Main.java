ff3b2c5ae008a96feec91c265897a8bdd6d00868
macolszewski <olschevski@gmail.com>
2018-08-12 10:29:42 +0200

    Add method to showing text

--- src/gitflow/Main.java
+++ src/gitflow/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        showText("TEST");
+    }
+
+    public static void showText(String text) {
+        System.out.println("Tekst: " + text);
     }
 }
